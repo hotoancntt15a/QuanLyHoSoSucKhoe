129a817dc734cb0dd4bc5de6f0e0487029ca4c90
Hồ Toàn <hotoancntt15a@hotmail.com>
2020-09-23 07:30:57 +0700

    Hoàn thành bước nhập hồ sơ bệnh nhân

--- QuanLyHoSoSucKhoe/Controllers/HoSoController.cs
+++ QuanLyHoSoSucKhoe/Controllers/HoSoController.cs
@@ -1,14 +1,8 @@
-﻿using NPOI.XSSF.Extractor;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
-using System.Web.Security;
-using System.Web.UI.WebControls;
 using zModules;
 using zModules.MSSQLServer;
 
@@ -31,9 +25,12 @@ namespace QuanLyHoSoSucKhoe.Controllers
         public ActionResult save()
         {
             string tmp = "", id = "";
+            var regDate = new Regex("^[0-3][0-9]/[0-1][0-9]/[1-9][0-9]{3}$");
             var item = new Dictionary<string, string>();
+            var tsqlNgheCV = new List<string>();
+            var tsqlBenh = new List<string>(); 
             try
-            { 
+            {
                 var fields = "id,pic,hoten,gioitinh,ngaysinh,cmndhochieu,capngay,noicap,hokhauthuongtru,noiohientai,nghenghiep,noicongtac,ngaycongtachientai,tiensubenhcuagiadinh,times,iduser,iduser2".Split(',');
                 foreach (var v in fields) { item[v] = Request.getValue(v); }
                 id = item["id"];
@@ -45,28 +42,68 @@ namespace QuanLyHoSoSucKhoe.Controllers
                 if (item["hokhauthuongtru"] == "") { throw new Exception("4. Hộ khẩu thường trú bỏ trống"); }
                 if (item["noiohientai"] == "") { throw new Exception("5. Nơi ở hiện tại bỏ trống"); }
                 /* Kiểm tra quy tắc */
-                var regDate = new Regex("^[0-3][0-9]/[0-1][0-9]/[1-9][0-9]{3}$");
-                if(regDate.IsMatch(item["ngaysinh"]) == false) { throw new Exception($"2.2 Ngày sinh không đúng định dạng {item["ngaysinh"]}"); }
-                if (item["capngay"] != "") {
+                if (regDate.IsMatch(item["ngaysinh"]) == false) { throw new Exception($"2.2 Ngày sinh không đúng định dạng {item["ngaysinh"]}"); }
+                if (item["capngay"] != "")
+                {
                     if (regDate.IsMatch(item["capngay"]) == false) { throw new Exception($"3.2 Ngày cấp không đúng định dạng {item["capngay"]}"); }
                 }
                 if (item["ngaycongtachientai"] != "")
                 {
                     if (regDate.IsMatch(item["ngaycongtachientai"]) == false) { throw new Exception($"8.Ngày bắt đầu vào học/làm việc tại đơn vị hiện nay không đúng định dạng {item["ngaycongtachientai"]}"); }
-                } 
+                }
                 if (item["id"] != "") { id = item["id"]; } else { item["id"] = Tools.getTimeSpanCurrent().ToString(); }
-                /* Soát danh sách */ 
+                /* Soát danh sách */
                 item.Remove("times");
-                if (id != "") { item["iduser2"] = $"{Session["iduser"]}"; item.Remove("iduser"); } 
+                if (id != "") { item["iduser2"] = $"{Session["iduser"]}"; item.Remove("iduser"); }
                 else { item["iduser"] = $"{Session["iduser"]}"; }
+
+                /* Lấy danh sách nghề, công việc */
+                var listNgay1 = Request.Form.GetValues("cvngay1");
+                var listNgay2 = Request.Form.GetValues("cvngay1");
+                var listCVBenh = Request.Form.GetValues("cvngay1");
+                for (var i = 0; i < listNgay1.Count(); i++)
+                {
+                    if (string.IsNullOrEmpty(listNgay1[i])) { break; }
+                    if (regDate.IsMatch(listNgay1[i]) == false) { throw new Exception($"9. Nghề, công việc trước đây - Từ ngày không đúng: {listNgay1[i]} - {listNgay2[i]} - {listCVBenh[i]}"); }
+                    if (string.IsNullOrEmpty(listNgay2[i]) == false) { throw new Exception($"9. Nghề, công việc trước đây - đến ngày không đúng: {listNgay1[i]} - {listNgay2[i]} - {listCVBenh[i]}"); }
+                    if (string.IsNullOrEmpty(listCVBenh[i])) { throw new Exception($"9. Nghề, công việc trước đây - Nghề, Công việc bỏ trống: {listNgay1[i]} - {listNgay2[i]} - {listCVBenh[i]}"); }
+                    tsqlNgheCV.Add($"(N'{item["id"]}-{i}',N'{item["id"]}',N'{listNgay1[i].getValueField()}', N'{listNgay2[i].getValueField()}', N'{listCVBenh[i].getValueField()}')");
+                }
+                /* Lấy tiểu sử bệnh */
+                listNgay1 = Request.Form.GetValues("tsnambenh");
+                listCVBenh = Request.Form.GetValues("tsbenh");
+                for (var i = 0; i < listNgay1.Count(); i++)
+                {
+                    if (string.IsNullOrEmpty(listNgay1[i])) { break; }
+                    if (Regex.IsMatch(listNgay1[i], "^[1-9][0-9]{3}$") == false) { throw new Exception($"11.1 Tiền sử bệnh - Năm không đúng: {listNgay1[i]} - {listCVBenh[i]}"); }
+                    if (string.IsNullOrEmpty(listCVBenh[i])) { throw new Exception($"11.1 Tiền sử bệnh - Bệnh bỏ trống: {listNgay1[i]} - {listCVBenh[i]}"); }
+                    /* Loai: 1 - Tiểu sử bệnh; 2- Tiểu sử bệnh nghề nghiệp */
+                    tsqlBenh.Add($"(N'{item["id"]}-1{i}',N'{item["id"]}',1,N'{listNgay1[i].getValueField()}', N'{listCVBenh[i].getValueField()}')");
+                }
+                /* Lấy tiểu sử bệnh nghề nghiệp */
+                listNgay1 = Request.Form.GetValues("tsnambenhnghe");
+                listCVBenh = Request.Form.GetValues("tsbenhnghe");
+                for (var i = 0; i < listNgay1.Count(); i++)
+                {
+                    if (string.IsNullOrEmpty(listNgay1[i])) { break; }
+                    if (Regex.IsMatch(listNgay1[i], "^[1-9][0-9]{3}$") == false) { throw new Exception($"11.2 Tiền sử bệnh nghề nghiệp - Năm không đúng: {listNgay1[i]} - {listCVBenh[i]}"); }
+                    if (string.IsNullOrEmpty(listCVBenh[i])) { throw new Exception($"11.2 Tiền sử bệnh nghề nghiệp - Bệnh bỏ trống: {listNgay1[i]} - {listCVBenh[i]}"); }
+                    /* Loai: 1 - Tiểu sử bệnh; 2- Tiểu sử bệnh nghề nghiệp */
+                    tsqlBenh.Add($"(N'{item["id"]}-2{i}',N'{item["id"]}',2,N'{listNgay1[i].getValueField()}', N'{listCVBenh[i].getValueField()}')");
+                }
             }
             catch (Exception ex) { return Content($"<div class=\"alert alert-danger\">{ex.saveMessage()}</div>"); }
             tmp = "";
             using (var db = local.getDataObject())
             {
                 try
-                {
+                {  
                     db.Execute(item, "hoso", id == "" ? "" : $"id=N'{id.getValueField()}'");
+                    db.ExecuteCommand($"delete from hosotsbenh where idhoso=N'{item["id"]}'; delete from hosotscongviec where idhoso=N'{item["id"]}'; ");
+                    var tsql = new List<string>();
+                    if (tsqlNgheCV.Count > 0) { tsql.Add($"insert into hosotscongviec (id,idhoso,ngay1,ngay2,congviec) values {tsqlNgheCV.ToJoin(",")}; "); }
+                    if (tsqlBenh.Count > 0) { tsql.Add($"insert into hosotsbenh (id,idhoso,loai,nam,tenbenh) values {tsqlBenh.ToJoin(",")}; "); }
+                    if (tsql.Count > 0) { db.ExecuteCommand(tsql.ToJoin(" ")); }
                 }
                 catch (Exception ex) { tmp = ex.saveMessage(); }
             }
